08fce31cfd00a5c8b7e0bf44532560651d811765
pedrombasso <pedro.basso@inf.ufrgs.br>
2020-10-31 15:21:49 +0100

    fixing more errors

--- nvbitfi/test-apps/cudaTensorCoreGemm/Parameters.h
+++ nvbitfi/test-apps/cudaTensorCoreGemm/Parameters.h
@@ -16,8 +16,8 @@
 
 struct Parameters {
 
-	// Parameters(int argc, char** argv);
-	// Parameters (); 
+	Parameters(int argc, char** argv);
+	
 
 	friend std::ostream& operator<<(std::ostream& os,
 			const Parameters& log_obj);
